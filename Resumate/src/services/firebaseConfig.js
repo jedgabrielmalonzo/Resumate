6888fb9ac4d72514b5490463882fd0ba829d2bb8
jedgabrielmalonzo <qjgmalonzo@tip.edu.ph>
2026-02-03 01:28:59 +0800

    inayos

---
+++ Resumate/src/services/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { initializeAuth} from "firebase/auth";    
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzsDW33HffRMm3OiNCVG1Z2E8FcCPqffE",
+  authDomain: "resumate-c9968.firebaseapp.com",
+  projectId: "resumate-c9968",
+  storageBucket: "resumate-c9968.firebasestorage.app",
+  messagingSenderId: "830045302104",
+  appId: "1:830045302104:web:5c04fd7a7ad06fee56892d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app);
+export default app;

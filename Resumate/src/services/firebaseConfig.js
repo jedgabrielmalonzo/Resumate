1de1047a7b520a5f226d5f292e22ba19481ffd17
jedgabrielmalonzo <qjgmalonzo@tip.edu.ph>
2026-02-04 01:43:57 +0800

    node modules

--- Resumate/src/services/firebaseConfig.js
+++ Resumate/src/services/firebaseConfig.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth} from "firebase/auth";    
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
